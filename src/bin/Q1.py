5a0f7433a9c78159d9da464cf97d6517a59045d8
RodAugustin <94622791+RodAugustin@users.noreply.github.com>
2023-04-21 15:07:52 +0200

    scound commit rod

--- src/bin/Q1.py
+++ src/bin/Q1.py
@@ -1,42 +1,60 @@
-# read a txt file
-# compare that file with a list of words
-# replace the words in the file with the "******"
-# write the new file to a new file
-
 def run_program():
-    sensitiv_word_list = []
-    read_sensitive_words_from_file(sensitiv_word_list)
+    display_info()
+
+    sensitive_word_list = []
+    read_sensitive_words_from_file(sensitive_word_list)
 
     random_text_list = []
     read_random_text_from_file(random_text_list)
 
-    redacted_text = []
-    compare_words(sensitiv_word_list, random_text_list, redacted_text)
+    redacted_text = compare_words_in_lists(sensitive_word_list, random_text_list)
 
-    print(redacted_text, end="")
+    with open("redacted_random_text.txt", "w") as file:
+        for line in redacted_text:
+            file.write(line + "\n")
+
+    print("Check out new redacted text file named; redacted_random_text")
+
+def display_info():
+    print("Welcome to the redacted text program.")
+    print("This is a program for Question of the exam")
+    print("-----------------------------------------")
 
 
+# A function that reads the "Sensitive words" from the text file and return the words in list.
+def read_sensitive_words_from_file(sensitive_word_list):
+    while True:
+        try:
+            filename = input("Type in the text file that contain the sensitive words: ")
+            with open(filename, "r") as file:
+                for word in file:
+                    sensitive_word_list.append(word.strip())
+            break
+        except FileNotFoundError:
+            print("File not found. Please try again.")
 
-def read_sensitive_words_from_file(sensitiv_word_list):
-    filename = "sensitive_words.txt"
-    file = open(filename, "r")
-    for word in file:
-        sensitiv_word_list.append(word)
-    file.close()
 
-
+# A function that reads some text from a text file and append it to a list.
 def read_random_text_from_file(random_text_list):
-    filename = "random_text.txt"
-    file = open(filename, "r")
-    for word in file:
-        random_text_list.append(word.strip())
-    file.close()
+    while True:
+        try:
+            filename = input("Type in the text file that needs redacting: ")
+            with open(filename, "r") as file:
+                for word in file:
+                    random_text_list.append(word.strip())
+            break
+        except FileNotFoundError:
+            print("File not found. Please try again.")
 
-def compare_words(sensitiv_word_list, random_text_list, redacted_text):
-    for word in random_text_list:
-        if word.lower() in  sensitiv_word_list:
-            redacted_text.append("******")
-        else:
-            redacted_text.append(word)
+
+# A function that compare and check if the words in sensitive word list is in the random text list and change the word if its sensitive.
+def compare_words_in_lists(sensitive_word_list, random_text_list):
+    redacted_text = []
+    for text in random_text_list:
+        for word in sensitive_word_list:
+            text = text.replace(word, '*' * len(word))
+        redacted_text.append(text)
+    return redacted_text
+
 
 run_program()
